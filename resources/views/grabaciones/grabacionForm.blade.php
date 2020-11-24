2e6159f6fc10d87866238b86ed59aaee6f8fba37
Samuel Mercado <samuel.mg@protonmail.com>
2020-11-24 17:05:31 -0600

    Extrae errores de formulario a vista parcial

--- resources/views/grabaciones/grabacionForm.blade.php
+++ resources/views/grabaciones/grabacionForm.blade.php
@@ -22,15 +22,7 @@
                 <h3 class="card-title">{{ isset($grabacion) ? 'Editar' : 'Crear' }} Grabación</h3>
             </div>
 
-            @if ($errors->any())
-                <div class="card-alert alert alert-warning mb-0">
-                    <ul class="m-0">
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
+            @include('partials.form-error')
 
             <div class="card-body">
                 @csrf
